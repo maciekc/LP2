79ff44f057404bce348f23fc713c37f713c5d860
Maciek <macceb01@gmail.com>
2017-10-16 20:41:31 +0200

    Poprawiona charakterystyka statyczna

--- MATLAB/Identyfikacja/pitch_moment.m
+++ MATLAB/Identyfikacja/pitch_moment.m
@@ -2,7 +2,7 @@ close all
 clear all
 v = [7.1 6.3 5.2 3.75 0 -5.15 -7.05 -8.7];
 masa = [0 15 30 45 60 75 90 105]; 
-moment = masa.*0.001*0.23*9.81;
+moment = masa.*0.001*0.26*9.81;
 x = -10:0.01:8;
 
 wsp = polyfit(v, moment, 3);
